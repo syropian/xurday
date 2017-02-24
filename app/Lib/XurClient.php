b5601ccaf0984baa87154e79f4cd88477731d081
syropian <collinpatrickhenderson@gmail.com>
2017-02-24 08:30:31 -0500

    Fixes!

--- app/Lib/XurClient.php
+++ app/Lib/XurClient.php
@@ -5,6 +5,7 @@ namespace XurDay\Lib;
 use XurDay\Exceptions\XurNotPresentException;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 
 class XurClient {
   protected $client;
@@ -19,21 +20,23 @@ class XurClient {
   public function getInventory() {
     $inventoryRes = $this->client->request('GET', 'Advisors/Xur/');
     $inventoryHash = json_decode($inventoryRes->getBody()->getContents(), true);
+    // dd($inventoryHash);
     if ($inventoryHash['ErrorStatus'] == 'DestinyVendorNotFound') {
       throw new XurNotPresentException();
     }
     $itemHashes = $this->getItemHashes($inventoryHash);
     $decodedItemHashes = $this->decodeItemHashes($itemHashes);
-    Cache::put('inventory', $decodedItemHashes, Carbon::parse('this friday', 'America/Los_Angeles')->addHours(3)->addMinutes(59));
+    $uniqueItemHashes = array_map('unserialize', array_unique(array_map('serialize', $decodedItemHashes)));
+    Cache::put('inventory', $uniqueItemHashes, Carbon::parse('next friday', 'America/Los_Angeles')->addHours(3)->addMinutes(59));
     return $decodedItemHashes;
   }
 
   private function getItemHashes($inventoryHash) {
     $itemHashes = [];
     $saleItemCategories = $inventoryHash['Response']['data']['saleItemCategories'];
-    foreach($itemCategory as $saleItemCategories) {
+    foreach($saleItemCategories as $itemCategory) {
       $saleItems = $itemCategory['saleItems'];
-      foreach($item as $saleItems) {
+      foreach($saleItems as $item) {
         $hashId = (string)$item['item']['itemHash'];
         $itemHashes[] = $hashId;
       }
@@ -44,16 +47,18 @@ class XurClient {
 
   private function decodeItemHashes($itemHashes) {
     $items = [];
-    foreach($hashId as $itemHashes) {
+    foreach($itemHashes as $hashId) {
       $hashRes = $this->client->request('GET', 'Manifest/6/'.$hashId);
       $hashBody = json_decode($hashRes->getBody()->getContents(), true);
       $itemName = $hashBody['Response']['data']['inventoryItem']['itemName'];
       $itemType = $hashBody['Response']['data']['inventoryItem']['itemTypeName'];
       $itemTier = $hashBody['Response']['data']['inventoryItem']['tierTypeName'];
+      $itemIcon = 'https://www.bungie.net'.$hashBody['Response']['data']['inventoryItem']['icon'];
       $items[] = [
         'name' => $itemName,
         'type' => $itemType,
-        'tier' => $itemTier
+        'tier' => $itemTier,
+        'icon' => $itemIcon
       ];
     }
 
